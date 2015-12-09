0cbcd64308d8fdb363a527594e1ab1cbeb1a0069
Julia Sivartsson <jsigc09@student.lnu.se>
2015-12-09 10:45:18 +0100

    Using filemtime() to check if cache or not

--- Laboration3/controller/MashupController.php
+++ Laboration3/controller/MashupController.php
@@ -3,12 +3,33 @@
 class MashupController
 {
     private $webService;
+    private static $fileName = 'response.json';
+    private $cache;
 
     public function __construct(){
+        $this->webService = new SverigesRadio();
     }
 
     public function doMashup(){
+        $webService = new SverigesRadio();
+        $traffic = null;
 
+
+        //Create a new request if file is older than 30 minute
+        if(time() - filemtime(self::$fileName) > 1800){
+            $traffic = $webService->getTrafficInformation();
+
+            if($traffic !== null){
+                $this->cache = fopen('response.json', 'w');
+                fwrite($this->cache, $traffic);
+                fclose($this->cache);
+            }
+            else{
+                $fileTime = date("j M Y H:i:s", filemtime(self::$fileName));
+                return "Just nu går det inte hämta trafikmeddelanden från Sveriges Radio. Hämtad {$fileTime}";
+            }
+        }
+        return "";
     }
 
 }
